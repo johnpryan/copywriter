2606102994fd5f79d8548d5ae1e3f8d989058bb3
John Ryan <ryjohn@google.com>
2020-06-12 11:42:44 -0700

    Run copywriter on itself.

--- lib/copywriter.dart
+++ lib/copywriter.dart
@@ -1,6 +1,6 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+// Copyright (c) 2020, John Ryan. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
 library copywriter;
 
 export 'src/copywriter_base.dart';
